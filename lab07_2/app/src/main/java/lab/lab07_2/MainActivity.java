8e9ab812530027cebb3eb9c3d2bf72b20111e519
Minho Kim <minho1a@hanmail.net>
2017-10-25 21:15:03 +0900

    Upload lab07-2

---
+++ lab07_2/app/src/main/java/lab/lab07_2/MainActivity.java
@@ -0,0 +1,88 @@
+package lab.lab07_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button_add;
+    private EditText edittext_search;
+    private ListView listview_singer;
+
+    private SingerAdapter singerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button_add = (Button) findViewById(R.id.button_add);
+        edittext_search = (EditText) findViewById(R.id.edittext_search);
+
+        listview_singer = (ListView) findViewById(R.id.listview_singer);
+        singerAdapter = new SingerAdapter() {{
+            addItem(new SingerItem(R.drawable.girlsgeneration, "소녀시대", "010-1234-5678", "2007"));
+            addItem(new SingerItem(R.drawable.apink, "에이핑크", "010-5678-1234", "2008"));
+            addItem(new SingerItem(R.drawable.girlfriend, "여자친구", "010-5233-6312", "2009"));
+            addItem(new SingerItem(R.drawable.redvelvet, "레드벨벳", "010-6322-7452", "2010"));
+            addItem(new SingerItem(R.drawable.aoa, "AOA", "010-1225-5321", "2011"));
+            addItem(new SingerItem(R.drawable.twice, "트와이스", "010-6663-5432", "2012"));
+        }};
+        listview_singer.setAdapter(singerAdapter);
+
+        listview_singer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                SingerItem item = (SingerItem) singerAdapter.getItem(i);
+                Toast.makeText(getApplicationContext(), "선택: " + item.getTeam(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private class SingerAdapter extends BaseAdapter {
+
+        ArrayList<SingerItem> items = new ArrayList<SingerItem>();
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return items.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            final SingerItem item = items.get(i);
+            SingerItemView singerItemView = new SingerItemView(MainActivity.this) {{
+                setIcon(item.getIcon());
+                setTeam(item.getTeam());
+                setNumber(item.getNumber());
+                setYear(item.getYear());
+            }};
+
+            return singerItemView;
+        }
+
+        public void addItem(SingerItem item) {
+            items.add(item);
+        }
+    }
+}
---
+++ lab07_2/app/src/main/java/lab/lab07_2/SingerItem.java
@@ -0,0 +1,48 @@
+package lab.lab07_2;
+
+public class SingerItem {
+
+    public int resId;
+    public String team;
+    public String number;
+    public String year;
+
+    public SingerItem(int resId, String team, String number, String year) {
+        this.resId = resId;
+        this.team = team;
+        this.number = number;
+        this.year = year;
+    }
+
+    public int getIcon() {
+        return resId;
+    }
+
+    public void setIcon(int resId) {
+        this.resId = resId;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
